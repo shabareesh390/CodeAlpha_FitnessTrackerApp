77d4f539f82cb1f6a723a9949d7c2b6d42f59cd5
Shabareesh S Gowda <shabareesh390@gmail.com>
2026-07-19 10:19:40 +0530

    removed the unused import for theme_provider.dart

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import '../../theme/app_colors.dart';
 import '../../theme/app_spacing.dart';
 import '../../providers/auth_provider.dart';
-import '../../providers/theme_provider.dart';
 import '../../providers/profile_provider.dart';
 import '../../widgets/glass_card.dart';
 import '../../animations/fade_animation.dart';
